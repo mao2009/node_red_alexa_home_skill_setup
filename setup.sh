6cd159f0f704a5330ae1714dc5e1d85c7434ec9b
mao2009 <en>
2018-08-24 15:14:42 +0900

    add sudo to pip install

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 sudo apt update
 sudo apt install git nodered npm python-pip -y
 
-pip install rmctl
+sudo pip install rmctl
 
 update-nodejs-and-nodered
 
 sudo systemctl start nodered
 sudo systemctl enable nodered
 
-sudo reboot
+until [ -e .node-red] ; do
+    sleep 1
+done
 cd .node-red
 sudo npm install node-red-contrib-alexa-home-skill
