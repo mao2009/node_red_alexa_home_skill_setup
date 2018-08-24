5c9340bf047266d1626e0db2397d3a4e2f0cb338
mao2009 <en>
2018-08-24 13:36:35 +0900

    braodlink_cliをrmctlに変更

--- setup.sh
+++ setup.sh
@@ -1,17 +1,13 @@
 sudo apt update
 sudo apt install git nodered npm python-pip -y
 
-git clone https://github.com/mao2009/broadlink_ctl
-sudo ln -s ./broadlink_ctl/broadlink_ctl.python.py broadlink.py
-
-pip install broadlink
-if [[ $(python --version) == "2*" ]];then
-    pip install configparser
-fi
+pip install rmctl
 
 update-nodejs-and-nodered
+
 sudo systemctl start nodered
 sudo systemctl enable nodered
+
 sudo reboot
 cd .node-red
 sudo npm install node-red-contrib-alexa-home-skill
