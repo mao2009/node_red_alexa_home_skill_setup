2b720d4c2f3475bff72be49934fd79b042dd4996
mao2009 <en>
2018-08-24 16:37:14 +0900

    シバンとメッセージを追加

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
-sudo apt update
-sudo apt install git nodered npm python-pip -y
+#!/usr/bin/env bash
 
+echo "#update and upgrade"
+sudo apt update
+sudo apt upgrade
+
+echo "#Install tools"
+sudo apt install git nodered npm python-pip -y
 sudo pip install rmctl
 
+echo "Update nodejs and nodred"
 update-nodejs-and-nodered
 
+echo "#Start nodered"
 sudo systemctl start nodered
 sudo systemctl enable nodered
 
@@ -12,4 +19,6 @@ until [ -e .node-red] ; do
     sleep 1
 done
 cd .node-red
-sudo npm install node-red-contrib-alexa-home-skill
+
+echo "#install alaxa home skill"
+npm install node-red-contrib-alexa-home-skill
